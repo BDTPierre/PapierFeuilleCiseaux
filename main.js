3a1eea2fd0a4d639d0460dac77553255fcd2d456
BDTPierre <buipierre@hotmail.fr>
2024-01-10 14:00:54 +0100

    Fonction indexOf + Proposer au joueur de jouer ou non des le debut

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 // Faire un shifumi contre l'utilisateur
 
-function replay() {
-
 let shifumi = ["Pierre", "Feuille", "Ciseaux"]
 let index = Math.floor(Math.random() * shifumi.length);
-console.log(index)
+// console.log(index)
 let robot = shifumi[index]
-console.log(robot)
-let player = window.prompt("Choisissez entre Pierre, Feuille, Ciseaux");
-console.log(player)
-        
-    if (player === robot) {
-        alert ("Vous avez une égalité, dommage!");
-        } else if ((player === "Pierre" && robot === "Ciseaux") || (player === "Ciseaux" && robot === "Feuille") || (player === "Feuille") && (robot === "Pierre")) {
-            alert ("Bravo, vous avez gagné!");
-            } else {
-                alert ("Dommage, vous avez perdu");
-                }
+// console.log(robot)
+let annuler = window.confirm ("Voulez-vous jouer?")
+
+function replay() {
+
+if (annuler === true){
+    let player = window.prompt("Choisissez entre Pierre, Feuille, Ciseaux");
+    let check = shifumi[shifumi.indexOf(player)]
+    while (player !== check || annuler === false) {
+        alert ("Veuillez choisir entre Pierre, Feuille, Ciseaux, s'il vous plaît")
+        // annuler = window.confirm ("Voulez-vous jouer?")
+        player = window.prompt("Choisissez entre Pierre, Feuille, Ciseaux") 
+        check = shifumi[shifumi.indexOf(player)]
+    }
+
+if (player === robot) {
+    alert ("Vous avez une égalité, dommage!");
+    } else if ((player === "Pierre" && robot === "Ciseaux") || (player === "Ciseaux" && robot === "Feuille") || (player === "Feuille") && (robot === "Pierre")) {
+        alert ("Bravo, vous avez gagné!");
+        } else {
+            alert ("Dommage, vous avez perdu");
+            }
 
 let infoplayer = window.confirm("Veux-tu rejouer ?")
 if(infoplayer === true){
@@ -24,6 +33,10 @@ if(infoplayer === true){
     } else {
         alert("Merci d'avoir joué !") 
     }
+
+} else {
+    alert("Aurevoir!")
+}
 }
 
 replay()
