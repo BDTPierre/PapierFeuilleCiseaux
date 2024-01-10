62b8efe9aa0e6f58d0cda31d5c61ba81a51e69d5
BDTPierre <buipierre@hotmail.fr>
2024-01-10 10:42:34 +0100

    Indentation

--- main.js
+++ main.js
@@ -10,20 +10,20 @@ console.log(robot)
 let player = window.prompt("Choisissez entre Pierre, Feuille, Ciseaux");
 console.log(player)
         
-if (player === robot) {
-    alert ("Vous avez une égalité, dommage!");
-} else if ((player === "Pierre" && robot === "Ciseaux") || (player === "Ciseaux" && robot === "Feuille") || (player === "Feuille") && (robot === "Pierre")) {
-    alert ("Bravo, vous avez gagné!");
-} else {
-    alert ("Dommage, vous avez perdu");
-}
+    if (player === robot) {
+        alert ("Vous avez une égalité, dommage!");
+        } else if ((player === "Pierre" && robot === "Ciseaux") || (player === "Ciseaux" && robot === "Feuille") || (player === "Feuille") && (robot === "Pierre")) {
+            alert ("Bravo, vous avez gagné!");
+            } else {
+                alert ("Dommage, vous avez perdu");
+                }
 
 let infoplayer = window.confirm("Veux-tu rejouer ?")
 if(infoplayer === true){
     replay ()
-} else {
-    alert("Merci d'avoir joué !") 
-}
+    } else {
+        alert("Merci d'avoir joué !") 
+    }
 }
 
 replay()
